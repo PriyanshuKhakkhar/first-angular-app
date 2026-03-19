698f0e7bd6ddc5694366bbd253d7a1d871513a75
Priyanshu Khakkhar <khakkharpriyanshu@gmail.com>
2026-03-19 17:59:17 +0530

    Practicing Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header';
+import { UserComponent } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,UserComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/header.ts
+++ src/app/header.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-header',
     standalone: true,
-    templateUrl: './header.html'
+    templateUrl: './header.html',
+    styleUrl: './header.css'
 })
 export class HeaderComponent{}
